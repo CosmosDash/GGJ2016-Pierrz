b9911b553fe01f603f48d43d7ebd6a56546f67a3
CosmosDash <philippe.lallement0@gmail.com>
2016-01-30 20:26:32 +0100

    Menu & Credit complets

---
+++ New Unity Project/Assets/scripts/Link.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Link : MonoBehaviour 
+{
+	public Canvas LinkCanvas;
+
+	public void LoadLink()
+	{
+		Application.OpenURL ("http://globalgamejam.org/2016/jam-sites/est-de-la-france-epitech-nancy");
+	}
+}
