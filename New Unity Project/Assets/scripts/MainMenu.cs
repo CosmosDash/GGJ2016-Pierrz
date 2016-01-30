7f929b1c63b11a5290988e6ed412c7ef8ef5bc2f
CosmosDash <philippe.lallement0@gmail.com>
2016-01-30 18:52:22 +0100

    Menu + build settings

---
+++ New Unity Project/Assets/scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public Canvas MainCanvas;
+
+	public void LoadGame()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void LoadCredits()
+	{
+		Application.LoadLevel (2);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
