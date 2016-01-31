2f9712c45aa6386c0f542313b99a10724e50712a
CosmosDash <philippe.lallement0@gmail.com>
2016-01-31 07:33:14 +0100

    Le monolith parle

---
+++ New Unity Project/Assets/scripts/AnimalsFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalsFollow : MonoBehaviour {
+
+	Transform leader;
+	float speed = 10;
+	float distMax = 10;
+	float distMin = 2;
+
+	// Use this for initialization
+	void Start () {
+		leader = GameObject.FindGameObjectWithTag("plaine").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (leader);
+		MoveIA();
+	}
+
+	void MoveIA () {
+		if (Vector3.Distance (transform.position, leader.position) >= distMin ) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+	}
+}
