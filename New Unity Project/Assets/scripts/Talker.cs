90d74d91965d4b6175e431c219045f60424b269a
RomainFranck <romain.franck@epitech.eu>
2016-01-31 04:21:26 +0100

    PD

---
+++ New Unity Project/Assets/scripts/Talker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Talker : MonoBehaviour
+{
+    public string[] lines;
+    public bool isTalking = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider p_other)
+    {
+       if (p_other.tag == "Player")
+        {
+            isTalking = true;
+        }
+    }
+
+    void OnTriggerExit(Collider p_other)
+    {
+        if (p_other.tag == "Player")
+        {
+            isTalking = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/scripts/TalkerTextComponent.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TalkerTextComponent : MonoBehaviour
+{
+    public Talker[] talkers;
+
+    bool m_areTalkersActive = false;
+    bool m_isTalking = false;
+
+    Text m_text;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_text = GetComponentInChildren<Text>();
+        m_text.text = string.Empty;
+
+        GetComponent<Image>().color = new Color(1, 1, 1, 0);
+    }
+
+    IEnumerator talk(Talker p_talker)
+    {
+        while (m_isTalking)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        m_isTalking = true;
+
+        GetComponent<Image>().color = new Color(1, 1, 1, 0.4f);
+
+        foreach (string line in p_talker.lines)
+        {
+            m_text.text = line;
+            yield return new WaitForSeconds(1.5f);
+        }
+
+        m_text.text = string.Empty;
+        GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
+        m_isTalking = false;
+
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Talker talker in talkers)
+        {
+            if (talker.isTalking)
+            {
+                StartCoroutine(talk(talker));
+                talker.isTalking = false;
+            }
+        }
+    }
+}
